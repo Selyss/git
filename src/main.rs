9e2e1267a4db6153564caf76e24f25b3e60ae709
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-03 12:14:55 -0500

    create directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+use std::path::Path;
+
+fn init(repo_name: &str) -> Result<(), std::io::Error> {
+    // create dir for repo and initialize .git/
+    if let Err(e) = fs::create_dir(repo_name) {
+        return Err(e);
+    }
+
+    // create .git/
+    let git_dir = repo_name.to_owned() + "/.git/";
+    if let Err(e) = fs::create_dir(git_dir) {
+        return Err(e);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match init("test_repo") {
+        Ok(_) => println!("Repository initialized successfully"),
+        Err(e) => eprintln!("Error initializing repository: {}", e),
+    }
 }
