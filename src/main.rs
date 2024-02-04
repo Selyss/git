2b6f7114233e08230e307487630ea663471e03a0
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-03 19:21:54 -0500

    spaghetti

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 use std::fs;
 use std::io::prelude::*;
+use std::io::Write;
+use std::path::Path;
 
 fn init(repo_name: &str) -> Result<(), std::io::Error> {
     // create dir for repo and initialize .git/
@@ -26,6 +31,39 @@ fn init(repo_name: &str) -> Result<(), std::io::Error> {
     }
 
     Ok(())
+}
+
+fn hash_object(data: Vec<u8>, obj_type: &str, write: bool) -> Result<String, std::io::Error> {
+    let header = format!("{} {}", obj_type, data.len()).into_bytes();
+    let all_data = [header, b"\x00".to_vec(), data].concat(); // FIXME: What.!
+
+    let mut hasher = Sha1::new();
+    hasher.update(&all_data);
+    let result = hasher.finalize();
+
+    if write == true {
+        let path_str = String::from_utf8([b".git/objects/", &result[..2], &result[2..]].concat());
+        let path_str = path_str.unwrap();
+        let path = Path::new(&path_str);
+
+        if !path.exists() {
+            if let Err(e) = fs::create_dir_all(path) {
+                return Err(e);
+            }
+            let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+            if let Err(e) = e.write_all(&all_data) {
+                return Err(e);
+            }
+            let compressed_data = e.finish().unwrap();
+
+            let file = fs::File::create(path);
+            if let Err(e) = file.unwrap().write_all(&compressed_data) {
+                return Err(e);
+            }
+        }
+    }
+
+    return Ok(format!("{:x}", result));
 }
 
 fn main() {
